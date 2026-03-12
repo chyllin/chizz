cd3fcc2f99983bab50177f84697153e044443136
Maame Esi <esiappeagyei12@gmail.com>
2026-03-12 17:15:21 +0000

    P to P

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,6 +1,6 @@
 import { motion } from "framer-motion";
 import { useCart } from "../context/CartContext";
-import "./ProductCard.css";
+import "./productCard.css";
 
 interface ProductCardProps {
   id: string;
